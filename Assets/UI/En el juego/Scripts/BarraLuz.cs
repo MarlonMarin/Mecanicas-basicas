7ff6b0f409dcd754db621b9de4b0c80cfb65975e
marlon marin <marlon.marin@uao.edu.co>
2018-11-17 16:39:44 -0500

    barra de vida y minimapa

---
+++ Assets/UI/En el juego/Scripts/BarraLuz.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BarraLuz : MonoBehaviour
+{
+
+    public Scrollbar barra;
+    public float luz = 100f;
+    public bool estado = true;
+    public float valorGana=10f;
+    public float valorPierde = 3f;
+
+    void Update()
+    {
+        if (estado)
+        {
+            if (luz >= 100f)
+            {
+                luz = 100f;
+            }
+            else
+            {
+                luz += valorGana * Time.deltaTime;
+            }
+            barra.value = luz / 100f;
+        }
+        else
+        {
+            if (luz <= 0)
+            {
+                luz = 0;
+            }
+            else
+            {
+                luz -= valorPierde * Time.deltaTime;
+            }
+            barra.value = luz / 100f;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="enLuz")
+        {
+            estado = true;
+        }
+        else if(other.tag == "enSombra")
+        {
+            estado = false;
+        }
+    }
+}
